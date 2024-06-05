2dc3f699d3a23377b2c873afd504710d6d7f3d1c
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-06-05 17:51:31 +0800

    optimized status buttons

--- src/App.jsx
+++ src/App.jsx
@@ -10,37 +10,38 @@ import ListItem from '@mui/material/ListItem';
 import ListSubheader from '@mui/material/ListSubheader';
 import ListItemText from '@mui/material/ListItemText';
 import { LineChart } from '@mui/x-charts/LineChart';
+import AddIcon from '@mui/icons-material/Add';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
+// function GitHubIcon() {
+//   return (
+//     <>
+//      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+//         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+//         class="feather feather-github">
+//         <path
+//           d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+//         </path>
+//       </svg>
 
-function GitHubIcon() {
-  return (
-    <>
-     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-        class="feather feather-github">
-        <path
-          d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
-        </path>
-      </svg>
-    
-    </>
-  )
-}
+//     </>
+//   )
+// }
 
 
 function Title() {
   return (
     <>
-       <div className='flex flex-row items-center justify-between w-full h-14'>
+      <div className='flex flex-row items-center justify-between w-full h-14'>
         <h1 className='text-3xl md:text-4xl font-bold'>
           RAR Client
         </h1>
-      
-      <Button variant='outlined' color='secondary' size='large' startIcon={<GitHubIcon />}>  GitHub</Button>
-      
+
+        <Button variant='outlined' color='secondary' size='large' startIcon={<GitHubIcon />}>  GitHub</Button>
+
       </div>
 
-    
+
     </>
   )
 }
@@ -85,17 +86,8 @@ function Top() {
       })
       .catch(error => console.error('Error deleting card:', error));
   };
-  async function Refresh() {
-    fetch('https://rarserver.lostmypillow.duckdns.org/api/cards')
-      .then(response => response.json())
-      .then(data => {
-        setCardList(data)
-        console.log(cardList)
-      })
-      .catch(error => console.error('Error fetching card list:', error));
-  }
 
-  useEffect(() => {
+  function GetData() {
     fetch('https://rarserver.lostmypillow.duckdns.org/api/attempts/total')
       .then(response => response.json())
       .then(data => setTotalAttempts(data.total_attempts))
@@ -115,24 +107,38 @@ function Top() {
       })
       .catch(error => console.error('Error fetching card list:', error));
 
+  }
 
+
+  function Refresh() {
+    fetch('https://rarserver.lostmypillow.duckdns.org/api/cards')
+      .then(response => response.json())
+      .then(data => {
+        setCardList(data)
+        console.log(cardList)
+      })
+      .catch(error => console.error('Error fetching card list:', error));
+  }
+
+  useEffect(() => {
+    GetData()
   }, []);
   return (
     <><div className='flex flex-col md:flex-row items-center justify-between gap-6 basis-1/3'>
-   
-      <div className='flex flex-row md:flex-col gap-4 w-full md:w-[50%] h-[15%] md:h-full'>
-        <Button variant="contained" size="large" disableElevation className='w-full h-full'>
-          <div className='text-3xl md:text-6xl'>
-            總請求數: {totalAttempts}
-          </div>
-        </Button>
-        <Button variant="contained" color="success" size="large" disableElevation className='w-full h-full'>
-          <div className='text-3xl md:text-6xl'>
-            今天請求數: {attemptsToday}
-          </div>
-        </Button>
-      </div>
 
+
+      <List className='w-full md:w-[50%] h-[15%] md:h-full' >
+        <ListSubheader>
+          Status
+        </ListSubheader>
+
+        <ListItem className='md:text-4xl'>
+          總請求數: {totalAttempts}
+        </ListItem>
+        <ListItem className='md:text-4xl'>
+          今天請求數: {attemptsToday}
+        </ListItem>
+      </List>
       <Card variant='outlined' className='flex flex-col gap-2 w-full md:w-[50%] border-2 border-black px-4 py-2'>
         <CardContent className='flex flex-col gap-4'>
           <List component="nav" subheader=
@@ -153,7 +159,7 @@ function Top() {
             onChange={handleInputChange} id="filled-basic" variant="filled" label="Add a new item" className='w-full' />
         </CardContent>
         <CardActions>
-          <Button size='large' variant='contained' className='w-full' onClick={handleAddItem}>
+          <Button size='large' variant='contained' className='w-full' onClick={handleAddItem} startIcon={<AddIcon />}>
             Add
           </Button>
         </CardActions>
